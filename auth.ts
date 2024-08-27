2fccc05577bf1ee47de0a769255805f7e9d059cb
Aaron Gray <aaronngray@gmail.com>
2024-08-27 18:44:00 +0100

    Initial commit of callback 404 issue.

---
+++ auth.ts
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+//import Google from "next-auth/providers/google"
+
+//import authConfig from "./auth.config"
+  
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  providers: [GitHub],
+  session: { strategy: "jwt" },
+//  ...authConfig
+})
